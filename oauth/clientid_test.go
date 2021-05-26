deb75e446616bcd6b8ef108286d3efab7dc6ce82
ZalgoNoise <zalgo.noise@gmail.com>
2021-05-26 19:31:21 +0200

    Adding multi-scope parsing tests

--- oauth/clientid_test.go
+++ oauth/clientid_test.go
@@ -2,6 +2,8 @@ package oauth
 
 import (
 	"testing"
+	// "fmt"
+	// "strings"
 )
 
 func TestNewClientID(t *testing.T) {
@@ -76,6 +78,52 @@ func TestNewClientID(t *testing.T) {
 		)
 		if err != nil && test.ok != false {
 			t.Errorf(`TestNewClientID(%q) = %q, expected error to be %v`, test.input, err, test.ok)
+		}
+	}
+}
+
+func TestClientIDAuthURL(t *testing.T) {
+	type input struct {
+		id           string
+		secret       string
+		scopes       string
+		refreshToken string
+	}
+
+	tests := []struct {
+		input input
+		want    string
+	}{
+		{
+			input: input{
+				id:           "ClientID",
+				secret:       "ClientSecret",
+				scopes:       "https://www.googleapis.com/auth/userinfo.email",
+				refreshToken: "",
+			},
+			want: "https://accounts.google.com/o/oauth2/auth?client_id=ClientID&redirect_uri=urn%3Aietf%3Awg%3Aoauth%3A2.0%3Aoob&response_type=code&access_type=offline&prompt=consent&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email",
+		},{
+			input: input{
+				id:           "ClientID",
+				secret:       "ClientSecret",
+				scopes:       "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile",
+				refreshToken: "",
+			},
+			want: "https://accounts.google.com/o/oauth2/auth?client_id=ClientID&redirect_uri=urn%3Aietf%3Awg%3Aoauth%3A2.0%3Aoob&response_type=code&access_type=offline&prompt=consent&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile",
+		},
+	}
+
+	for _, test := range tests {
+		clientID, _ := NewClientID(
+			test.input.id,
+			test.input.secret,
+			test.input.scopes,
+			test.input.refreshToken,
+		)
+
+		clientID.RefreshToken.SetAuthURL(clientID)
+		if clientID.RefreshToken.GetAuthURL() != test.want {
+			t.Errorf(`TestClientIDAuthURL(%q) = %q, expected result to be %q`, test.input.scopes, clientID.RefreshToken.GetAuthURL(), test.want)
 		}
 	}
 }
